99230e6652c41b023d6b20f4e6ef2c6adb1c7e4a
Kannspeki <noier.fort@gmail.com>
2022-05-10 20:02:55 -0300

    Added command line options
     Removed the interactive mode in place of CLI

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # PySword
-A simple python password generator  
+# A simple Python Password Generator  
 
-## Usage
+### Usage
 
-```./pysword```  
-* Choose the password length, any positive integer    
-```password lenght: ```  
-* Select the type of password, multiple choices supported e.g. ```ns``` for numbers and symbols  
-```(n)umbers, (l)etters, (s)ymbol:```  
-* Output  
-```%*4!5|!```  
+```usage: pysword.py [-h] [-n] [-a] [-s] length```
+
+positional arguments:  
+  length        The length of the Password  
+
+options:  
+  -h, --help    show this help message and exit  
+  -n, --number  Generate only Numbers  
+  -a, --alpha   Generate only alphabetical characters  
+  -s, --symbol  Generate only symbols  
+
+### Examples
+* A password with 5 characters made of numbers:
+```pysword.py 5 -n```  
+```48139```
+* 5 characters made of numbers and symbols:
+```pysword.py 5 -ns```   
+```8#"<4``` 
+* And to combine all three
+```pysword.py 10 -nsa```  
+```Z3U|T```
+
+
+
--- pysword.py
+++ pysword.py
@@ -1,49 +1,61 @@
 #!/usr/bin/env python3
-# 2022-05-05 A simple python password generator  
 
-import random, string
+import random, string, argparse, sys
 
-lenght = int(input('password lenght: '))
-print('Choose options:')
-choice = input(('(n)umbers, (l)etters, (s)ymbol: '))
-choice = choice.lower()
+parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Python Password Generator')
+parser.add_argument('length', type=int, help='The length of the Password')
 
-options = ['nsl', 'nls', 'snl', 'sln', 'lsn', 'lns']
+parser.add_argument('-n', '--number', action="store_true", help='Generate only Numbers')
+parser.add_argument('-a', '--alpha', action="store_true", help='Generate only alphabetical characters')
+parser.add_argument('-s', '--symbol', action="store_true", help='Generate only symbols')
 
-for i in range(lenght):
 
-    if choice == 'n':
+args = parser.parse_args()
+
+length = args.length
+options = ['-nsa', '-nas', '-sna', '-san', '-asn', '-ans']
+
+for i in range(length):
+
+    if len(sys.argv) < 2 and len(sys.argv) > 3:
+        exit()
+
+    if '-n' in sys.argv or '--number'in sys.argv:
         num = string.digits
         numpsw = "".join(random.sample(num, 1))
         print(numpsw, end="")
-    if choice == 'l':    
+
+    if '-a' in sys.argv or '--alpha' in sys.argv:
         letters = string.ascii_letters
         letpsw = random.choice(letters)
         print(letpsw, end="")
-    if choice == 's':    
+
+    if '-s' in sys.argv or '--symbol' in sys.argv:    
         pon = string.punctuation
         popsw = random.choice(pon) 
         print(popsw, end="")
 
-    if choice == 'ns' or choice == 'sn':
+    if '-ns' in sys.argv or '-sn' in sys.argv:
         ns = string.digits + string.punctuation
         nspsw = "".join(random.sample(ns, 1))
         print(nspsw, end="")
 
-    if choice == 'nl' or choice == 'ln':
+    if '-na' in sys.argv or '-an' in sys.argv:
         nl = string.digits + string.ascii_letters
         nlpws = "".join(random.sample(nl, 1)) 
         print(nlpws, end="")
 
-    if choice == 'ls' or choice == 'sl':
+    if '-as' in sys.argv or '-sa' in sys.argv:
         ls = string.ascii_letters + string.punctuation
         lspsw = "".join(random.sample(ls, 1))
         print(lspsw, end="")
     
-    if len(choice) == 3 and choice in options: 
+    if len(sys.argv[2]) == 4:
         nls = string.digits + string.ascii_letters + string.punctuation
         nlspsw = "".join(random.sample(nls, 1))
         print(nlspsw, end="")
     
 print("")
 exit
+
